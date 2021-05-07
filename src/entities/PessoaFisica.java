5114e312998c494f74184644cabc3794592322d5
Natália Tavares <natalia.h.tavares@gmail.com>
2021-05-07 11:37:29 -0300

    criando classe PessoaFisica herdando atributos e método imposto() de Contribuinte

---
+++ src/entities/PessoaFisica.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class PessoaFisica extends Contribuinte{
+	
+	private Double gastosSaude;
+
+	public PessoaFisica(String nome, Double rendaAnual, Double gastosSaude) {
+		super(nome, rendaAnual);
+		this.gastosSaude = gastosSaude;
+	}
+	
+	public Double getGastosSaude() {
+		return gastosSaude;
+	}
+
+	public void setGastosSaude(Double gastosSaude) {
+		this.gastosSaude = gastosSaude;
+	}
+
+	@Override
+	public double imposto() {
+		return 0;
+	}
+}
